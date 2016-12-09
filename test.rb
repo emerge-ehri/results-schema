22a5e2555398eb0d0b0032a592be0e3480504c87
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-12-09 15:09:47 -0600

    Specify eMERGE namespace URI

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 def validate(document_path, schema_path)
   schema = Nokogiri::XML::Schema(File.read(schema_path))
   document = Nokogiri::XML(File.read(document_path))
-  document.root.add_namespace_definition(nil, "")
+  document.root.add_namespace_definition(nil, "http://emerge.mc.vanderbilt.edu/")
   schema.validate(document)
 end
 
